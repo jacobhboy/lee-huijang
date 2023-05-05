e2a9496d8fd9aa24191e5f3dc1d61013dc26dcff
이창보 <101192100+jacobhboy@users.noreply.github.com>
2023-05-05 22:36:09 +0900

    aaa

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Box from "./components/Box";
+import Header from "./components/Header"
+import axios from "axios";
 
 function App() {
+  axios.get('api/qp5x7acdh0nszyqya7g6u724qknr7vp40rzq6ee7vr3lxkcnmnjv6q53g4zfh/', )
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Box 
+
+      />
     </div>
   );
 }
